42f5126b97fc43f40c11f76ea6d951f47600e70b
aatsvetkov <aatsvetkov17@gmail.com>
2024-01-31 00:59:35 +0300

    's001'

--- ellie.py
+++ ellie.py
@@ -5,4 +5,12 @@ class Ellie(object):
     def __init__(self) -> None:
         self.name = 'Ellie'
         self.version = 2
+        self.run = True
+
+    def cmd(self):
+        while self.run:
+            command = input("~e> ")
+            if command == 'stop':
+                self.run = False
         
+e = Ellie().cmd()
